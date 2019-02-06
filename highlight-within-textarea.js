dd5b7c219800418e75b1cb1230cea9f1532cf220
Matthieu Felix <matthieufelix@gmail.com>
2019-02-05 23:06:47 -0500

    Removing jQuery
    Some stuff broke 😢

--- highlight-within-textarea.js
+++ highlight-within-textarea.js
@@ -8,13 +8,13 @@
 (function($) {
 	let ID = 'hwt';
 
-	let HighlightWithinTextarea = function($el, config) {
-		this.init($el, config);
+	let HighlightWithinTextarea = function(el, config) {
+		this.init(el, config);
 	};
 
 	HighlightWithinTextarea.prototype = {
-		init: function($el, config) {
-			this.$el = $el;
+		init: function(el, config) {
+			this.el = el[0];
 
 			// backwards compatibility with v1 (deprecated)
 			if (this.getType(config) === 'function') {
@@ -54,20 +54,27 @@
 		},
 
 		generate: function() {
-			this.$el
-				.addClass(ID + '-input ' + ID + '-content')
-				.on('input.' + ID, this.handleInput.bind(this))
-				.on('scroll.' + ID, this.handleScroll.bind(this));
+			console.log(this.el);
+			this.el.classList.add(ID + '-input', ID + '-content');
+			this.el.addEventListener('input.' + ID, this.handleInput.bind(this));
+			this.el.addEventListener('scroll.' + ID, this.handleScroll.bind(this));
 
-			this.$highlights = $('<div>', { class: ID + '-highlights ' + ID + '-content' });
+			// TODO instead of calling on `document`, do it on the root element
 
-			this.$backdrop = $('<div>', { class: ID + '-backdrop' })
-				.append(this.$highlights);
+			this.highlights = document.createElement('div');
+			this.highlights.classList.add(ID + '-highlights', ID + '-content');
 
-			this.$container = $('<div>', { class: ID + '-container' })
-				.insertAfter(this.$el)
-				.append(this.$backdrop, this.$el) // moves $el into $container
-				.on('scroll', this.blockContainerScroll.bind(this));
+			this.backdrop = document.createElement('div');
+			this.backdrop.classList.add(ID + '-backdrop');
+			this.backdrop.appendChild(this.highlights);
+
+			this.container = document.createElement('div');
+			this.container.classList.add(ID + '-container');
+
+			this.el.insertAdjacentElement('afterend', this.container);
+			this.container.appendChild(this.backdrop);
+			this.container.appendChild(this.el);
+			this.container.addEventListener('scroll', this.blockContainerScroll.bind(this));
 
 			this.browser = this.detectBrowser();
 			switch (this.browser) {
@@ -102,22 +109,24 @@
 			}
 		},
 
+		// TODO
 		// Firefox doesn't show text that scrolls into the padding of a textarea, so
 		// rearrange a couple box models to make highlights behave the same way
 		fixFirefox: function() {
+			return;
 			// take padding and border pixels from highlights div
-			let padding = this.$highlights.css([
+			let padding = this.highlights.css([
 				'padding-top', 'padding-right', 'padding-bottom', 'padding-left'
 			]);
-			let border = this.$highlights.css([
+			let border = this.highlights.css([
 				'border-top-width', 'border-right-width', 'border-bottom-width', 'border-left-width'
 			]);
-			this.$highlights.css({
+			this.highlights.css({
 				'padding': '0',
 				'border-width': '0'
 			});
 
-			this.$backdrop
+			this.backdrop
 				.css({
 					// give padding pixels to backdrop div
 					'margin-top': '+=' + padding['padding-top'],
@@ -134,17 +143,19 @@
 				});
 		},
 
+		// TODO
 		// iOS adds 3px of (unremovable) padding to the left and right of a textarea,
 		// so adjust highlights div to match
 		fixIOS: function() {
-			this.$highlights.css({
+			return;
+			this.highlights.css({
 				'padding-left': '+=3px',
 				'padding-right': '+=3px'
 			});
 		},
 
 		handleInput: function() {
-			let input = this.$el.val();
+			let input = this.el.value;
 			let ranges = this.getRanges(input, this.highlight);
 			let unstaggeredRanges = this.removeStaggeredRanges(ranges);
 			let boundaries = this.getBoundaries(unstaggeredRanges);
@@ -280,7 +291,7 @@
 		},
 
 		renderMarks: function(boundaries) {
-			let input = this.$el.val();
+			let input = this.el.value;
 			boundaries.forEach(function(boundary, index) {
 				let markup;
 				if (boundary.type === 'start') {
@@ -292,7 +303,7 @@
 			});
 
 			// this keeps scrolling aligned when input ends with a newline
-			input = input.replace(/\n(\{\{hwt-mark-stop\}\})?$/, '\n\n$1');
+			input = input.replace(/\n({{hwt-mark-stop}})?$/, '\n\n$1');
 
 			// encode HTML entities
 			input = input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
@@ -303,8 +314,8 @@
 			}
 
 			// replace start tokens with opening <mark> tags with class name
-			input = input.replace(/\{\{hwt-mark-start\|(\d+)\}\}/g, function(match, submatch) {
-				var className = boundaries[+submatch].className;
+			input = input.replace(/{{hwt-mark-start\|(\d+)}}/g, function(match, submatch) {
+				let className = boundaries[+submatch].className;
 				if (className) {
 					return '<mark class="' + className + '">';
 				} else {
@@ -313,31 +324,31 @@
 			});
 
 			// replace stop tokens with closing </mark> tags
-			input = input.replace(/\{\{hwt-mark-stop\}\}/g, '</mark>');
+			input = input.replace(/{{hwt-mark-stop}}/g, '</mark>');
 
-			this.$highlights.html(input);
+			this.highlights.innerHTML = input;
 		},
 
 		handleScroll: function() {
-			let scrollTop = this.$el.scrollTop();
-			this.$backdrop.scrollTop(scrollTop);
+			this.backdrop.scrollTop = this.el.scrollTop;
 
 			// Chrome and Safari won't break long strings of spaces, which can cause
 			// horizontal scrolling, this compensates by shifting highlights by the
 			// horizontally scrolled amount to keep things aligned
-			let scrollLeft = this.$el.scrollLeft();
-			this.$backdrop.css('transform', (scrollLeft > 0) ? 'translateX(' + -scrollLeft + 'px)' : '');
+			let scrollLeft = this.el.scrollLeft;
+			this.backdrop.style.transform = (scrollLeft > 0) ? 'translateX(' + -scrollLeft + 'px)' : '';
 		},
 
 		// in Chrome, page up/down in the textarea will shift stuff within the
 		// container (despite the CSS), this immediately reverts the shift
 		blockContainerScroll: function() {
-			this.$container.scrollLeft(0);
+			this.container.scrollLeft = 0;
 		},
 
+		// TODO
 		destroy: function() {
-			this.$backdrop.remove();
-			this.$el
+			this.backdrop.remove();
+			this.el
 				.unwrap()
 				.removeClass(ID + '-text ' + ID + '-input')
 				.off(ID)
